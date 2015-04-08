2fa6358c136b498867f57b4e987515d87b5f2b81
Brenda Yang <t-fayang@outlook.com>
2015-04-08 17:35:44 +0800

    Code change for error resported by JSLint

--- PeoplePickerClient/AadDataProvider.js
+++ PeoplePickerClient/AadDataProvider.js
@@ -1,39 +1,39 @@
 ﻿(function () {
-    AadDataProvider = function() {
-    }
+    "use strict";
+
+    AadDataProvider = function () {
+    };
 
     AadDataProvider.prototype = {
         maxResult: 50,
         lastErrorMessage: null,
         severHost: 'yihcaow1001:3000',
         getPrincipals: function (input, callback) {
-            var xhr = new XMLHttpRequest();
+            var xhr = new XMLHttpRequest(), self = this;
             xhr.open('GET', 'http://' + this.serverHost + '/api?keyword=' + encodeURIComponent(input), true);
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.withCredentials = true;
-            var self = this;
             xhr.onabort = xhr.onerror = xhr.ontimeout = function () {
                 self.lastErrorMessage = 'Search error. Try login first.';
                 callback('Error', null);
             };
             xhr.onload = function () {
-                if (xhr.status == 401) {
+                if (xhr.status === 401) {
                     self.lastErrorMessage = 'Unauthorized. You need login first.';
                     callback('Unauthorized', null);
                     return;
                 }
-                else if (xhr.status != 200) {
+                if (xhr.status !== 200) {
                     self.lastErrorMessage = 'Unknown error. Status code: ' + xhr.statusCode;
                     callback('Unknown error', null);
                 }
-                var result = JSON.parse(xhr.responseText);
-                if ("odata.error" in result) {
-                    self.lastErrorMessage = 'Server error: ' + result["odata.error"]["message"]["value"];
+                var result = JSON.parse(xhr.responseText), people = [];
+                if (result["odata.error"] !== undefined) {
+                    self.lastErrorMessage = 'Server error: ' + result["odata.error"].message.value;
                     callback(result["odata.error"], null);
                     return;
                 }
-                var people = new Array();
-                result["value"].forEach(
+                result.value.forEach(
                     function (e) {
                         var person = {};
                         person.DisplayName = e.displayName;
@@ -42,13 +42,13 @@
                         people.push(person);
                     });
                 if (people.length > self.maxResult) {
-                    people = people.slice(0, self.maxResult)
+                    people = people.slice(0, self.maxResult);
                 }
                 callback(null, people);
             };
             xhr.send('');
         }
-    }
+    };
 })();
 
 
