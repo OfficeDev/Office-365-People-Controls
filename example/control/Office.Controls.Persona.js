edd08fdd54152c6b6687fd28f6b7c1b7c5e55578
Cathy <missair@live.com>
2015-06-03 07:06:55 +0800

    Change template layout & event handling for action bar

--- example/control/Office.Controls.Persona.js
+++ example/control/Office.Controls.Persona.js
@@ -12,86 +12,134 @@
         }
     }
 
+    /*
+    *  {
+            ImageUrl: 'control/images/icon.png',
+            PrimaryText: '***REMOVED*** Chen',
+            SecondaryText: 'Software Engineer 2, ASG EA China', // JobTitle, Department
+            TertiaryText: 'BEIJING-BJW-1/12329', // Office
+            Email: '***REMOVED***@microsoft.com',
+            SipAddress: '***REMOVED***@microsoft.com',
+            MobilePhone: '+86 1861-2947-014',
+            WorkPhone: '+86(10) 59173216',
+            Id: '***REMOVED***',
+        };
+    */
     Office.Controls.Persona = function (root, templateID, dataProvider, isHidden) {
-        try {
-            if (typeof root !== 'object' || typeof dataProvider !== 'object' || (Office.Controls.Utils.isNullOrUndefined(templateID))) {
+        if (typeof root !== 'object' || typeof dataProvider !== 'object' || (Office.Controls.Utils.isNullOrUndefined(templateID))) {
                 Office.Controls.Utils.errorConsole('Invalid parameters type');
                 return;
             }
             
-            this.root = root;
-            this.templateID = templateID;
-            this.dataProvider = dataProvider;
-            this.isHidden = isHidden;
-            this.currentNode = null;
-
-            this.loadTemplate();
-        } catch (ex) {
-            throw ex;
-        }
+        this.root = root;
+        this.templateID = templateID;
+        this.dataProvider = dataProvider;
+        this.isHidden = isHidden;
     };
 
     Office.Controls.Persona.prototype = {
-        delaySearchInterval: 300,
         onError: null,
-        dataProvider: null,
-        showErrors: true,        
-        errorMessageElement: null,
-        alertDiv: null,
-        currentToken: null,
-        widthSet: false,
-        hasErrors: false,
-        errorDisplayed: null,
-        isCreated: true,
+        rootNode: null,
+        mainNode: null,
+        actionNodes: null,
+        actionDetailNodes: null,
         oriID: "",
 
-        getErrorDisplayed: function () {
-            return this.errorDisplayed;
+        get_rootNode: function() {
+            return this.rootNode;
         },
 
-        loadTemplate: function ()
+        get_mainNode: function() {
+            return this.mainNode;
+        },
+
+        get_actionNodes: function() {
+            return this.actionNodes;
+        },
+
+        get_actionDetailNodes: function() {
+            return this.actionDetailNodes;
+        },
+
+        loadTemplateAsync: function (templatePath, callback)
         {
+            if (Office.Controls.Utils.isNullOrUndefined(templatePath)) {
+                Office.Controls.Utils.errorConsole('Wrong template path');
+                return;
+            }
+
             var self = this;
             var xmlhttp = new XMLHttpRequest();
-            xmlhttp.open("GET", "control/templates/template.htm", true);
+            xmlhttp.open("GET", templatePath, true);
             xmlhttp.onreadystatechange = function() {
                 // if (this.readyState !== 4) return;
                 // if (this.status !== 200) return; // or whatever error handling you want
-                if (this.readyState === 4)
-                {
-                    if (this.status === 200)
-                    {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
                         var parser, xmlDoc
-                        if (window.DOMParser)
-                        {
+                        if (window.DOMParser) {
                            parser = new DOMParser();
                            xmlDoc = parser.parseFromString(this.responseText,"text/xml");
-                        }
-                        else // code for IE
-                        {
+                        } else { // code for < IE9
                            xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
                            xmlDoc.async = false;
                            xmlDoc.loadXML(this.responseText); 
                         }  
-                        
-                        var templateNode = xmlDoc.getElementById(self.templateID);
-                        var templateElement = document.createElement("div");
-                        self.hiddenNode(templateElement, self.isHidden);
-
-                        // Do data binding
-                        templateElement.innerHTML = self.bindData(templateNode.innerHTML);
-
-                        self.root.appendChild(templateElement);
-                        self.currentNode = templateElement;
+                        self.parseTemplate(xmlDoc);
+                        callback(self.rootNode, null); 
                     }
                 }
-                else
-                {
-                    // error message
-                }
-                
             };
             xmlhttp.send();
+        },
+
+        parseTemplate: function (xmlDoc)
+        {
+            try {
+                if (typeof xmlDoc !== 'object' || (Office.Controls.Utils.isNullOrUndefined(xmlDoc))) {
+                    Office.Controls.Utils.errorConsole('Invalid template document');
+                    return;
+                }
+                
+                var templateNode = xmlDoc.getElementById(this.templateID);
+                var templateElement = document.createElement("div");
+                this.hiddenNode(templateElement, this.isHidden);
+
+                // Do data binding
+                templateElement.innerHTML = this.bindData(templateNode.innerHTML);
+                if ((Office.Controls.Utils.isNullOrUndefined(templateElement))) {
+                    Office.Controls.Utils.errorConsole('Fail to get persona document');
+                    return;
+                }
+                this.root.appendChild(templateElement);
+                this.rootNode = templateElement;
+
+                // Get main node
+                this.mainNode = templateElement.getElementsByClassName(Office.Controls.PersonaConstants.SectionTag_Main);
+                if (this.mainNode === null) {
+                    this.mainNode = this.rootNode;
+                } else {
+                    // Get action nodes
+                    this.actionNodes = templateElement.getElementsByClassName(Office.Controls.PersonaConstants.SectionTag_Action);
+                    if (this.actionNodes !== null) {
+                        // Get actiondetail nodes
+                        this.actionDetailNodes = templateElement.getElementsByClassName(Office.Controls.PersonaConstants.SectionTag_ActionDetail);
+                        // Add click event to show the action detail node
+                        var node = null;
+                        var self = this;
+                        for (var i = 0; i < self.actionNodes.length; i++) {
+                            if (self.actionNodes[i] !== null) {
+                                node = self.actionNodes[i];
+                                Office.Controls.Utils.addEventListener(node, 'click', function (e) {
+                                    self.setActiveStyle(event);
+                                });
+                            }
+                        }
+                    }
+                }
+            } catch (ex) {
+                throw ex;
+            }
         },
 
         bindData: function (htmlStr)
@@ -99,13 +147,13 @@
             var regExp = /\$\{([^\}\{]+)\}/g;
             var resultStr = htmlStr;
             // Get the data
-            var personaInfo = this.dataProvider;
+            var displayInfo = this.dataProvider;
 
             // Get the property names
             var properties = resultStr.match(regExp);
 
             for (var i = 0; i < properties.length; i++) { 
-                var propertyValue = personaInfo[properties[i].substring(2, properties[i].length - 1)]
+                var propertyValue = displayInfo[properties[i].substring(2, properties[i].length - 1)]
                 resultStr = resultStr.replace(properties[i], propertyValue);
             }
 
@@ -128,39 +176,44 @@
 
         removeDetailCard: function ()
         {
-           /* var root = document.getElementById("root");
-            root.parentNode.removeChild(currentNode);*/
         },
 
-        setActiveStyle: function (event, childID)
+        setActiveStyle: function (event)
         {
             // Get the element triggers the event
             var e = event || window.event;
-            var targetElement = e.target || e.srcElement;
+            // var currentNode = e.target || e.srcElement;
+            var currentNode = e.currentTarget;
 
-            var changedClassName = " is-active";
-            var parentClassName = Office.Controls.PersonaConstants.ClassName_PersonaCardAction;
-            var childClassName = PersonaConstants.ClassName_PersonaCardActionDetails;
+            var changedClassName = "is-active";
+            var childClassName = currentNode.getAttribute('child');
 
-            var parentElements = document.getElementsByClassName(parentClassName);
-            for (var i = 0; i < parentElements.length; i++) {
-                parentElements[i].className = parentElements[i].className.replace(changedClassName, "");
+            for (var i = 0; i < this.actionNodes.length; i++) {
+                if ((currentNode === this.actionNodes[i])) {
+                    if (this.oriID !== childClassName) {
+                        Office.Controls.Utils.addClass(this.actionNodes[i], changedClassName); 
+                        this.oriID = childClassName;
+                    } else {
+                        this.oriID = "";
+                        Office.Controls.Utils.removeClass(this.actionNodes[i], changedClassName); 
+                    }
+                } else {
+                    Office.Controls.Utils.removeClass(this.actionNodes[i], changedClassName);
+                }
             }
-            var childElements = document.getElementsByClassName(childClassName);
-            for (var i = 0; i < childElements.length; i++) {
-                childElements[i].className = childElements[i].className.replace(changedClassName, "");
-            }
 
-            if ((targetElement.className.indexOf(changedClassName) == -1) || (oriID != childID)) {
-                targetElement.className += changedClassName;
-                var srcElement = document.getElementById(childID);
-                srcElement.className += changedClassName;  
-                this.oriID = childID;
-            } else {
-                this.oriID = "";
+            for (var i = 0; i < this.actionDetailNodes.length; i++) {
+                if (Office.Controls.Utils.containClass(this.actionDetailNodes[i], childClassName) && (this.oriID === childClassName)) {
+                    Office.Controls.Utils.addClass(this.actionDetailNodes[i], changedClassName); 
+                } else {
+                    Office.Controls.Utils.removeClass(this.actionDetailNodes[i], changedClassName);
+                } 
             }
         },
     };
+
+    Office.Controls.Persona.Strings = function() {
+    }
 
     Office.Controls.PersonaResources = function () {
     };
@@ -169,10 +222,20 @@
     };
 
     if (Office.Controls.Persona.registerClass) { Office.Controls.Persona.registerClass('Office.Controls.Persona'); }
+    // if (Office.Controls.Persona.PersonaViewModel.registerClass) {Office.Controls.Persona.PersonaViewModel.registerClass('Office.Controls.Persona.PersonaViewModel');}
+    if (Office.Controls.Persona.Strings.registerClass) { Office.Controls.Persona.Strings.registerClass('Office.Controls.Persona.Strings'); }
     if (Office.Controls.PersonaConstants.registerClass) { Office.Controls.PersonaConstants.registerClass('Office.Controls.PersonaConstants'); }
     if (Office.Controls.PersonaResources.registerClass) { Office.Controls.PersonaResources.registerClass('Office.Controls.PersonaResources'); }
-    Office.Controls.Persona.res = {};
-    Office.Controls.PersonaConstants.ClassName_PersonaCardAction = "ms-PersonaCard-action";
-    Office.Controls.PersonaConstants.ClassName_PersonaCardActionDetails = "ms-PersonaCard-actionDetails";
     Office.Controls.PersonaResources.PersonaName = 'Persona';
+    Office.Controls.Persona.Strings.emailString = 'Email';
+    Office.Controls.Persona.Strings.lyncMessageString = 'IM';
+    Office.Controls.Persona.Strings.phoneString = 'Phone';
+    Office.Controls.Persona.Strings.mobileString = 'Mobile';
+    Office.Controls.Persona.Strings.workPhoneString = 'Work';
+    Office.Controls.Persona.Strings.colonString = ':';
+    Office.Controls.Persona.Strings.suspensionPoints = '...';
+    Office.Controls.Persona.res = {};
+    Office.Controls.PersonaConstants.SectionTag_Main = "persona-section-tag-main";
+    Office.Controls.PersonaConstants.SectionTag_Action = "ms-PersonaCard-action";
+    Office.Controls.PersonaConstants.SectionTag_ActionDetail = "ms-PersonaCard-actionDetails";
 })();
--- example/control/PersonaSample.js
+++ example/control/PersonaSample.js
@@ -1,29 +1,63 @@
 function showAll()
-{
+{	
+	var tempPath = "control/templates/template.htm";
 	var dataProvider = samplePersonInfo();
 	var root = document.getElementById('nameOnlyRoot');
 	var nameOnly = new Office.Controls.Persona(root, 'NameOnly', dataProvider, true);
-	Office.Controls.Utils.addEventListener(nameOnly.root, 'click', function (e) {
-        return new Office.Controls.Persona(root, 'NameImage', dataProvider, true);
+	var nameImage = null;
+	var isShow = true;
+
+	nameOnly.loadTemplateAsync(tempPath, function (rootNode, error) {
+        if (rootNode !== null) {
+            Office.Controls.Utils.addEventListener(rootNode, 'click', function (e) {
+            	if (nameImage == null) {
+            		nameImage = new Office.Controls.Persona(root, 'NameImage', dataProvider, true);
+		        	nameImage.loadTemplateAsync(tempPath, function (rootNode, error) {
+		        		isShow = false;
+		        	});	
+            	} else {
+            		nameImage.hiddenNode(nameImage.get_rootNode(), isShow);
+            		isShow = isShow ? false : true;
+            	}
+		    });
+        } else {
+            // error handling
+        }
     });
 	
 	var pcRoot = document.getElementById('personaCardRoot');
 	var personaCard = new Office.Controls.Persona(pcRoot, 'PersonaCard', dataProvider, true);
+	personaCard.loadTemplateAsync(tempPath, function (rootNode, error) {
+
+	});
 }
 
 
 function samplePersonInfo()
 {
     // User Profile
-    var personaInfo = new Array(); 
-    personaInfo["DisplayName"] = "***REMOVED*** Chen"; 
-    personaInfo["JobTitle"] = "Software Engineer 2"; 
-    personaInfo["ImageUrl"] = "control/images/icon.png"; 
-    personaInfo["Department"] = "ASG EA China";
-    personaInfo["Office"] = "BEIJING-BJW-1/12329"; 
-    personaInfo["Email"] = "***REMOVED***@microsoft.com"; 
-    personaInfo["SipAddress"] = "***REMOVED***@microsoft.com"; 
-    personaInfo["MobilePhone"] = "+86 1861-2947-014"; 
-    personaInfo["WorkPhone"] = "+86(10) 59173216"; 
-    return personaInfo;
+    // return personaInfo = {
+    //     DisplayName: '***REMOVED*** Chen',
+    //     JobTitle: 'Software Engineer 2',
+    //     ImageUrl: 'control/images/icon.png',
+    //     Department: 'ASG EA China',
+    //     Office: 'BEIJING-BJW-1/12329',
+    //     Email: '***REMOVED***@microsoft.com',
+    //     SipAddress: '***REMOVED***@microsoft.com',
+    //     MobilePhone: '+86 1861-2947-014',
+    //     WorkPhone: '+86(10) 59173216',
+    //     PersonId: '***REMOVED***',
+    // };
+
+    return displayInfo = {
+            ImageUrl: 'control/images/icon.png',
+            PrimaryText: '***REMOVED*** Chen',
+            SecondaryText: 'Software Engineer 2, ASG EA China', // JobTitle, Department
+            TertiaryText: 'BEIJING-BJW-1/12329', // Office
+            Email: '***REMOVED***@microsoft.com',
+            SipAddress: '***REMOVED***@microsoft.com',
+            MobilePhone: '+86 1861-2947-014',
+            WorkPhone: '+86(10) 59173216',
+            Id: '***REMOVED***',
+        };
 }
