2e8e02bc37e764067db98069b30f4f67e5811ee9
Cathy <missair@live.com>
2015-06-10 06:17:28 -0700

    Persona:Add Demo that integrates with ADD User Data

--- src/Office.Controls.PeopleAadDataProvider.js
+++ src/Office.Controls.PeopleAadDataProvider.js
@@ -14,16 +14,70 @@
     }
 
     Office.Controls.PeopleAadDataProvider = function () {
-        this.authContext = authContext;
     }
 
     Office.Controls.PeopleAadDataProvider.prototype = {
         maxResult: 50,
+        authContext: null,
         lastErrorMessage: null,
         severHost: 'localhost:3000', // local debug
         acquireToken: '00000002-0000-0000-c000-000000000000', // AAD resource key
         apiVersion: 'api-version=1.5', 
 
+        getPrincipals: function (keyword, callback) {
+            var xhr = new XMLHttpRequest(), self = this;
+            xhr.open('GET', 'http://' + this.serverHost + '/api?keyword=' + encodeURIComponent(keyword), true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.withCredentials = true;
+            xhr.onabort = xhr.onerror = xhr.ontimeout = function () {
+                self.lastErrorMessage = 'Search error. Try login first.';
+                callback('Error', null);
+            };
+            xhr.onload = function () {
+                if (xhr.status === 401) {
+                    self.lastErrorMessage = 'Unauthorized. You need login first.';
+                    callback('Unauthorized', null);
+                    return;
+                }
+                if (xhr.status !== 200) {
+                    self.lastErrorMessage = 'Unknown error. Status code: ' + xhr.statusCode;
+                    callback('Unknown error', null);
+                }
+                var result = JSON.parse(xhr.responseText), people = [];
+                if (result["odata.error"] !== undefined) {
+                    self.lastErrorMessage = 'Server error: ' + result["odata.error"].message.value;
+                    callback(result["odata.error"], null);
+                    return;
+                }
+                result.value.forEach(
+                    function (e) {
+                        var person = {};
+                        person.displayName = e.displayName;
+                        person.department = e.department;
+                        person.jobTitle = e.jobTitle;
+                        person.mail = e.mail;
+                        person.workPhone = e.telephoneNumber;
+                        person.mobile = e.mobile;
+                        person.office = e.physicalDeliveryOfficeName;
+                        person.sipAddress = e.userPrincipalName;
+                        person.alias = e.mailNickname;
+                        person.personId = e.objectId;
+                        people.push(person);
+                    });
+                if (people.length > self.maxResult) {
+                    people = people.slice(0, self.maxResult);
+                }
+                callback(null, people);
+            };
+            xhr.send('');
+        },
+
+        /**
+         * CORS
+         * @param  {[type]}
+         * @param  {Function}
+         * @return {[type]}
+         */
         getUsersAsync: function (keyword, callback) {
 
             var self = this;
